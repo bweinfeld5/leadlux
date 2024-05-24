73efeab0ac55593aabb8effc0a8cb59e071537be
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-24 15:47:37 -0400

    update

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import openpyxl
 
 def get_facebook_posts(page_id, access_token):
     url = f"https://graph.facebook.com/v13.0/{page_id}/posts"
@@ -16,18 +17,35 @@ def get_facebook_posts(page_id, access_token):
         print(f"Other error occurred: {err}")
     return None
 
-def print_posts(posts_data):
+def save_posts_to_excel(posts_data, filename):
+    # Create a new Excel workbook and select the active worksheet
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = 'Facebook Posts'
+    
+    # Write headers to the first row
+    headers = ['Time', 'Message']
+    sheet.append(headers)
+
+    # Write post data to the worksheet
     if posts_data:
         for post in posts_data.get('data', []):
-            print(f"Time: {post['created_time']}")
-            print(f"Message: {post.get('message', 'No message')}\n")
-    else:
-        print("No posts data found.")
+            time = post['created_time']
+            message = post.get('message', 'No message')
+            sheet.append([time, message])
+    
+    # Save the workbook
+    workbook.save(filename)
+    print(f"Data saved to {filename}")
 
 # Replace with your Facebook page ID and access token
 page_id = '1891279731302318'
 access_token = 'EAANzmAQY8k8BO02bBDnpPFgnnR5PuXH3cLhJkrbItWFlvCbwQSQZBjBq7W79nloSQ3ZBHOgmSi6uPQ2ByncM28ZA8CZA0nCR5tPOOiWgiD2cTscuJX8Ks0oKxBEeF3GqAGVDG7gOUIxDm2e0w73b5WDOqp80MG2h2I42QGLk9ZBIcycmVqW4wiNL3m905eJmsDpupLVQP9ib5yFsEzEXEtTz3YIxZBx81iac8ZD'
 
-# Retrieve and print posts from the Facebook page
+# Retrieve posts from the Facebook page
 posts_data = get_facebook_posts(page_id, access_token)
-print_posts(posts_data)
+
+# Save the posts data to an Excel file
+if posts_data:
+    save_posts_to_excel(posts_data, 'facebook_posts.xlsx')
+    
